9eb6fdbb354c22e1219472d412b2534443781408
darko <ki14.skakun@gmail.com>
2023-10-20 11:07:00 +0300

    user logout

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
 
     path('', views.home, name="home"),
     path('room/<str:id>/', views.room, name="room"),
