22518a99d1f2ccda470e20cf31ad147bfa42cf53
darko <ki14.skakun@gmail.com>
2023-10-21 09:27:24 +0300

    activity feed, refactoring components

--- base/views.py
+++ base/views.py
@@ -72,14 +72,16 @@ def home(request):
 
     topics = Topic.objects.all()
     room__count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room__count': room__count}
+    context = {'rooms': rooms, 'topics': topics, 'room__count': room__count,
+               'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, id):
     room = Room.objects.get(id=id)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
